407e4d3e8bb30e678fd07e5f4186e54ca5a32779
githubaccount77 <christophertubman@outlook.com>
2023-06-28 13:47:56 -0400

    CT Changelog 6/28/23

---
+++ .vs/MyFirstProgram.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace MyFirstProgram
+{
+  -references
+  class MyFirstProgram
+  (
+    -references
+    static void Main(string[] args)
+    (
+      Console.WriteLine("Hello World!");
+    )
+  )
+}
