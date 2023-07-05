4cdfcb266119bd4b502d222845c7257b6833175c
githubaccount77 <christophertubman@outlook.com>
2023-07-05 12:12:45 -0400

    CT Changelog 7/5/23

--- .vs/MyFirstProgram.cs
+++ .vs/MyFirstProgram.cs
@@ -753,5 +753,339 @@ namespace MyFirstProgram
     }
 }
 
+22.) Calculator Program
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                double num1 = 0;
+                double num2 = 0;
+                double result = 0;
+
+                Console.WriteLine("------------------");
+                Console.WriteLine("Calculator Program");
+                Console.WriteLine("------------------");
+
+                Console.Write("Enter number 1: ");
+                num1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                num2 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter an option: ");
+                Console.WriteLine("\t+ : Add");
+                Console.WriteLine("\t- : Subtract");
+                Console.WriteLine("\t* : Multiply");
+                Console.WriteLine("\t/ : Divide");
+                Console.Write("Enter an option: ");
 
 
+                switch (Console.ReadLine())
+                {
+                    case "+":
+                        result = num1 + num2;
+                        Console.WriteLine($"Your result: {num1} + {num2} = " + result);
+                        break;
+                    case "-":
+                        result = num1 - num2;
+                        Console.WriteLine($"Your result: {num1} - {num2} = " + result);
+                        break;
+                    case "*":
+                        result = num1 * num2;
+                        Console.WriteLine($"Your result: {num1} * {num2} = " + result);
+                        break;
+                    case "/":
+                        result = num1 / num2;
+                        Console.WriteLine($"Your result: {num1} / {num2} = " + result);
+                        break;
+                    default:
+                        Console.WriteLine("That was not a valid option");
+                        break;
+                }
+                Console.Write("Would you like to continue? (Y = yes, N = No): ");
+            } while (Console.ReadLine().ToUpper() == "Y");
+
+            Console.WriteLine("Bye!");
+            Console.ReadKey();
+        }
+    }
+}
+
+
+23.) Arrays 
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // array = a variable that can store multiple values. fixed size
+
+            //String[] cars = {"BMW", "Mustang", "Corvette"};
+
+            String[] cars = new string[3];
+
+            cars[0] = "Tesla";
+            cars[1] = "Mustang";
+            cars[2] = "Corvette";
+
+            for (int i = 0; i < cars.Length; i++)
+            {
+                Console.WriteLine(cars[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+24.) Foreach Loop
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // foreach loop = a simpler way to iterate over an array, but it's less flexible
+
+            String[] cars = {"BMW", "Mustang", "Corvette"};
+
+            foreach (String car in cars)
+            {
+                Console.WriteLine(car);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+25.) Methods
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method  = performs a section of code, whenever it's called "invoked".
+            //           benefit = Let's us reuse code w/o writing it multiple times
+            //           Good practice is to capitalize method names (I forgot in this video)
+
+            String name = "Bro";
+            int age = 21;
+
+            SingHappyBirthday(name, age);
+
+            Console.ReadKey();
+        }
+        static void SingHappyBirthday(String birthdayBoy, int yearsOld)
+        {
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine("Happy birthday dear " + birthdayBoy);
+            Console.WriteLine("You are " + yearsOld + " years old!");
+            Console.WriteLine("Happy birthday to you!");
+            Console.WriteLine();
+        }
+    }
+}
+
+26.) Return Keyword
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // return  = returns data back to the place where a method is invoked
+
+            double x;
+            double y;
+            double result;
+
+            Console.WriteLine("Enter in number 1: ");
+            x = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter in number 2: ");
+            y = Convert.ToDouble(Console.ReadLine());
+
+            result = Multiply(x, y);
+
+            Console.WriteLine(result);
+
+            Console.ReadKey();
+        }
+        static double Multiply(double x, double y)
+        {
+            return x * y;
+        }
+    }
+}
+
+27.) Method Overloading
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // method overloading  = methods share same name, but different parameters
+            //                       name + parameters = signature
+            //                       methods must have a unique signature
+
+            double total;
+            
+            total = Multiply(2, 3, 4);
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+
+        static double Multiply(double a, double b)
+        {      
+            return a * b;
+        }
+        static double Multiply(double a, double b, double c)
+        {
+            return a * b * c;
+        }
+    }
+}
+
+28.) Params Keyword
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //params keyword = a method parameter that takes a variable number of arguments.
+            //                 The parameter type must be a single - dimensional array
+
+            double total = CheckOut(3.99, 5.75, 15, 1.00, 10.25);
+
+            Console.WriteLine(total);
+            Console.ReadKey();
+        }
+
+        static double CheckOut(params double[] prices)
+        {
+            double total = 0;
+
+            foreach (double price in prices)
+            {
+                total += price;
+            }
+            return total;
+        }
+
+    }
+}
+
+29.) Exception Handling
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // exception = errors that occur during execution
+
+            //        try     = try some code that is considered "dangerous"
+            //        catch   = catches and handles exceptions when they occur
+            //        finally = always executes regardless if exception is caught or not
+
+            int x;
+            int y;
+            double result;
+
+            try
+            {
+                Console.Write("Enter number 1: ");
+                x = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Enter number 2: ");
+                y = Convert.ToInt32(Console.ReadLine());
+
+                result = x / y;
+
+                Console.WriteLine("result: " + result);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Enter ONLY numbers PLEASE!");
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("You can't divide by zero! IDIOT!");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong!");
+            }
+            finally
+            {
+                Console.WriteLine("Thanks for visiting!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
+
+
+30.) Conditional Operator
+
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // conditional operator = used in conditional assignment if a condition is true/false
+
+            //(condition) ? x : y
+
+            double temperature = 20;
+            String message;
+
+            message = (temperature >= 15) ? "It's warm outside!" : "It's cold outside!";
+
+            Console.WriteLine(message);
+
+            Console.ReadKey();
+        }
+    }
+}
+
+31.) String Interpolation
